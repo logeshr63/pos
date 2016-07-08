d6faa334b25f49041b301bddf15957ba5ceb15cf
logeshr63 <logeshr63@gmail.com>
2016-07-08 10:10:06 +0530

    Update and rename README.md to neg.c

---
+++ neg.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+void main()
+{
+int a;
+printf("enter the num",a);
+if(a>0)
+{
+printf("the num is positive");
+}
+elseif
+{
+printf("the num is negative");
+}
+else
+{
+printf("the num is zero");
+}}
